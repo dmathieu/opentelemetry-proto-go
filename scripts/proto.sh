2cb6707ceebe0cdc44082fed07155936fbb51e43
Damien Mathieu <42@dmathieu.com>
2021-08-19 16:58:41 +0200

    add auto-update action

--- scripts/proto.sh
+++ scripts/proto.sh
@@ -7,7 +7,7 @@ git clone https://github.com/open-telemetry/opentelemetry-proto $TMP_DIR
 cd $TMP_DIR
 
 for file in $(find . -name '*.proto'); do
-	sed -i '' 's,go_package = "github.com/open-telemetry/opentelemetry-proto/gen/go,go_package = "github.com/dmathieu/opentelemetry-proto-go,' $file
+	sed -i 's,go_package = "github.com/open-telemetry/opentelemetry-proto/gen/go,go_package = "github.com/dmathieu/opentelemetry-proto-go,' $file
 done
 
 make gen-go
