91aec2af54ecb7ff335097c36bc87afca42ba6b3
Damien Mathieu <42@dmathieu.com>
2021-08-19 16:55:59 +0200

    setup project

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/dmathieu/opentelemetry-proto-go
+
+go 1.16
+
+require (
+	github.com/gogo/protobuf v1.3.2
+	github.com/golang/protobuf v1.5.2
+	github.com/grpc-ecosystem/grpc-gateway v1.16.0
+	google.golang.org/grpc v1.40.0
+)
---
+++ scripts/proto.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -e
+
+DIR=`pwd`
+TMP_DIR=`mktemp -d`
+
+git clone https://github.com/open-telemetry/opentelemetry-proto $TMP_DIR
+cd $TMP_DIR
+
+for file in $(find . -name '*.proto'); do
+	sed -i '' 's,go_package = "github.com/open-telemetry/opentelemetry-proto/gen/go,go_package = "github.com/dmathieu/opentelemetry-proto-go,' $file
+done
+
+make gen-go
+
+cp -R $TMP_DIR/gen/go/github.com/dmathieu/opentelemetry-proto-go/* $DIR
